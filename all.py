608f14cd79049858dbb2fbde4282ddb9d3128109
lupengliang <1559540409@qq.com>
2023-08-07 21:43:57 +0800

    2023.08.07 新增主函数运行测试命令

--- all.py
+++ all.py
@@ -1,5 +1,5 @@
 import pytest
 
-
+# 建议干掉pytest.ini, 会有干拢
 if __name__ == '__main__':
-   pytest.main(["-sv", "testwhite", "-n=2"])
+   pytest.main(["-sv", "./testwhite::test_hum::TestHum::test_01_dafashi", "-n=1"])
